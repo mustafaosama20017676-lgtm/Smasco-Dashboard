b96aaf12d318efff2c0c6429a3919ad3203d90af
mustafaosama20017676-lgtm <mustafaosama20017676@gmail.com>
2026-02-16 01:40:13 +0300

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,8 @@
 import { io } from "socket.io-client";
 
 // We no longer send a JWT; just connect to the server
-const SERVER_URL = "https://bserver-6dg1.onrender.com";
+const SERVER_URL = "https://smasco-server.onrender.com";
 export const socket = io(SERVER_URL, {
   transports: ["websocket"],
 });
+
